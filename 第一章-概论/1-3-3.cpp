56a43d9deb8f4735a2f38014a4b35c5c4a2da18f
caoshenghui <csh0518@live.com>
2020-05-01 14:02:53 +0800

    commit

---
+++ 第一章-概论/1-3-3.cpp
@@ -0,0 +1,41 @@
+// File:    1-3-3.cpp
+// Author:  csh
+// Date:    2020/05/01
+// ===================
+
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+#define arrsize 300
+#define maxint INT_MAX
+
+
+// 非递归
+bool Algo(int i, int a[]){
+    if(i < 1 || (i > arrsize))
+        return false;
+    for(int j = 1; j <= i; j++){
+        if(j == 1)
+            a[j-1] = 2;
+        else{
+            if(a[j-2]*2*j > maxint)
+                return false;
+            a[j-1] = a[j-2] * 2 * j;
+        }
+    }
+    return true;
+}
+
+
+int main(){
+    int i = 3;
+    int a[arrsize];
+    Algo(i, a);
+    for(int j = 0; j < 5; j++)
+        cout << a[j] << " ";
+    cout << endl;
+    return 0;
+}
