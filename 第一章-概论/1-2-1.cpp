d0ba66449b37cb802df625d03b2e58075169ce98
caoshenghui <csh0518@live.com>
2020-05-13 22:51:33 +0800

    有问题

--- 第一章-概论/1-2-1.cpp
+++ 第一章-概论/1-2-1.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 
+
 class strings{
     public:
         string str;
@@ -19,33 +20,82 @@ class strings{
         strings(string s){str = s;};
 };
 
-int compareStr(char a, char b);
+bool compare(strings a, strings b);
+
+class ArrList{
+    private:
+        strings *aList;
+        int curlen;
+    public:
+        ArrList(){
+            curlen = 0; 
+            aList = NULL;
+        }
+        int length(){return curlen;}        // 链表长度
+        int compareStr(char a, char b){
+            int ret;
+            // 1. a,b相等
+            if(a-b == 0) ret = 0;
+
+            // 2. a,b都属于A-Z或都属于0-9
+            if(((a-'A' >= 0) && (b-'A' >= 0)) || ((a-'A' <= 0) && (b-'A' <= 0))){
+                if(a-b > 0) ret = 1;
+            }else{
+                if(a-b > 0) ret = -1;
+            }
+            return ret;
+        }
+        bool compare(strings *a, strings *b){
+            string i = a->str;
+            string j = b->str;
+            for(int k = 0; i[k] && j[k]; k++){
+                int ret = compareStr(i[k], j[k]);
+                if(ret == 0) continue;
+                if(ret == 1) return true;
+                if(ret == -1) return false;
+            }
+            return true;
+        }
+        void append(strings *s){
+            strings *current = aList;
+            strings *previous = NULL;
+            bool stop = false;
+            while(current != NULL && not stop){
+                if(compare(current, s))
+                    stop = true;
+                else{
+                    previous = current;
+                    current = current->pNext;
+                }
+            }
+            if(previous == NULL){
+                s->pNext = aList;
+                aList = s;
+            }else{
+                s->pNext = current;
+                previous->pNext = s;
+            }
+        }
+        void printAll(){
+            strings *tmp = aList;
+            while(tmp){
+                cout << tmp->str << endl;
+                tmp = tmp->pNext;
+            }
+        }
+};
+
+
 
 int main(){
     strings a[8] = {strings("ABC"), strings("5C"), strings("PABC"), strings("CXY"),
                     strings("CRSI"), strings("7"), strings("B899"), strings("B9")};
-
-    strings head = a[0];
-    a[0].pNext = NULL;
-    for(int i = 0; i < 8; i++){
-        int ret;            // 判别排序
-        for(int j = 0; a[i].str[j] && (a[i-1].str[j]); j++){
-            int ret = compareStr(a[i].str[j], a[i-1].str[j]);
-        }
+    ArrList list = ArrList();
+    for(int i = 0; i < 4; i++){
+        list.append(&a[i]);
     }
+    list.printAll();
+
     return 0;
 }
 
-int compareStr(char a, char b){
-    int ret;
-    // 1. a,b相等
-    if(a-b == 0) ret = 0;
-
-    // 2. a,b都属于A-Z或都属于0-9
-    if(((a-'A' >= 0) && (b-'A' >= 0)) || ((a-'A' <= 0) && (b-'A' <= 0))){
-        if(a-b > 0) ret = 1;
-    }else{
-        if(a-b > 0) ret = -1;
-    }
-    return ret;
-}
