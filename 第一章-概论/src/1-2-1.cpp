4a54ce3c3bdac08e446de43614b862d8e00ba500
caoshenghui <csh0518@live.com>
2020-07-21 16:42:31 +0800

    update 1-2-1.cpp

--- 第一章-概论/src/1-2-1.cpp
+++ 第一章-概论/src/1-2-1.cpp
@@ -1,98 +1,112 @@
 // File:    1-2-1.cpp
 // Author:  csh
-// Date:    2020/05/01
+// Date:    2020/07/20
 // ===================
 
 
-// 未做完，学完再补
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 
-
+// 1. 用链表
 class strings{
     public:
-        string str;
-        strings *pNext;
+        string str;             // 数据域
+        strings *pNext;         // 指针域
         strings();
         strings(string s){str = s;};
 };
 
-
-class ArrList{
+/* 链表类 */
+class LinkList{
     private:
-        strings *aList;
+        strings *head;
         int curlen;
     public:
-        ArrList(){
+        LinkList(){
             curlen = 0;
-            aList = NULL;
+            head = NULL;
         }
-        int length(){return curlen;}        // 链表长度
-        int compareStr(char a, char b){
-            int ret;
-            // 1. a,b相等
-            if(a-b == 0) ret = 0;
-
-            // 2. a,b都属于A-Z或都属于0-9
-            if(((a-'A' >= 0) && (b-'A' >= 0)) || ((a-'A' < 0) && (b-'A' < 0))){
-                if(a-b > 0) ret = 1;
-            }else{
-                if(a-b > 0) ret = -1;
-            }
-            return ret;
-        }
-        bool compare(string a, string b){
-            for(int k = 0; a[k] && b[k]; k++){
-                int ret = compareStr(a[k], b[k]);
-                if(ret == 0) continue;
-                if(ret == 1) return true;
-                if(ret == -1) return false;
-            }
-            return true;
-        }
-        void append(strings *s){
-            strings *current = aList;
-            strings *previous = NULL;
-            bool stop = false;
-            while(current != NULL && not stop){
-                if(compare(current->str, s->str) == true)
-                    stop = true;
-                else{
-                    previous = current;
-                    current = current->pNext;
-                }
-            }
-            if(previous == NULL){
-                s->pNext = aList;
-                aList = s;
-            }else{
-                s->pNext = current;
-                previous->pNext = s;
-            }
-        }
-        void printAll(){
-            strings *tmp = aList;
-            while(tmp){
-                cout << tmp->str << endl;
-                tmp = tmp->pNext;
-            }
-        }
+        int length(){return curlen;};       // 链表长度
+        bool compare(string a, string b);   // 比较a,b字符串
+        void append(strings *s);            // 添加元素并排序
+        void showAll();                     // 显示所有元素
 };
 
+bool LinkList::compare(string a, string b){
+    // 返回false表示a在前，b在后
+    auto i = a.begin();
+    auto j = b.begin();
+    while(i != a.end() && j != b.end()){
+        // 1.字符相等情况
+        if(*i == *j){
+            i++;
+            j++;
+            continue;
+        }
+
+        // 2.字符均为A-Z或均为0-9情况
+        if((*i - 'A' >= 0 && (*j - 'A' >= 0)) || ((*i - 'A' < 0) && (*j - 'A' < 0))){
+            if(*i - *j < 0)     // 如B-A>0,A在B前
+                return false;
+            else
+                return true;
+        }else{
+        // 3.一个字符A-Z，一个0-9
+            if(*i - *j > 0)     // 如A-5>0,A在5前
+                return false;
+            else return true;
+        }
+        i++;
+        j++;
+    }
+    if(*i)      // 如果a没有迭代完，b迭代完了，b在a前
+        return true;
+    else
+        return false;
+}
+
+void LinkList::append(strings *s){
+    strings *current = head;
+    strings *previous = NULL;
+    bool stop = false;
+    while(current != NULL && not stop){
+        if(compare(current->str, s->str))
+            stop = true;
+        else{
+            previous = current;
+            current = current->pNext;
+        }
+    }
+    if(previous == NULL){
+        // previous没找到，即插入第一个
+        s->pNext = head;
+        head = s;
+    }else{
+        //在previos和current之间插入
+        s->pNext = current;
+        previous->pNext = s;
+    }
+}
+
+void LinkList::showAll(){
+    strings *tmp = head;
+    while(tmp){
+        cout << tmp->str << " --> ";
+        tmp = tmp->pNext;
+    }
+}
 
 
 int main(){
-    strings a[8] = {strings("ABC"), strings("5C"), strings("PABC"), strings("CXY"),
+    strings a[8] = {strings("PAB"), strings("5C"), strings("PABC"), strings("CXY"),
                     strings("CRSI"), strings("7"), strings("B899"), strings("B9")};
-    ArrList list = ArrList();
+    LinkList list = LinkList();
     for(int i = 0; i < 8; i++){
         list.append(&a[i]);
     }
-    list.printAll();
-
+    list.showAll();
     return 0;
 }
-
